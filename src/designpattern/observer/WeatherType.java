132335506a17c4c5d596e7effadaeeb679b95baf
vquochuy <quochuy0925@gmail.com>
2019-03-18 21:00:22 +0700

    init enum weather type

---
+++ src/designpattern/observer/WeatherType.java
@@ -0,0 +1,10 @@
+package designpattern.observer;
+
+public enum WeatherType {
+    SUNNY, RAINY, WINDY, COLD;
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase();
+    }
+}
